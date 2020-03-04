e0c524625b16b0e3e2123a38595088bf342e98bc
jakubsvetlik <jabkoteam@gmail.com>
2020-03-04 18:38:54 +0100

    update

--- testcafe/quora/pages/login.po.js
+++
@@ -1,7 +0,0 @@
-import { Selector } from 'testcafe';
-
-export default class Login{
-    pass = Selector(".form_column").nth(1);
-    email = Selector(".form_column").nth(0);
-    button = Selector("input[value='Login']");
-};
